5c631ba042c87cc240807d95dcbbdb255d6f92c2
m.tanzeel.ext@tahaluf.ae <m.tanzeel.ext@tahaluf.ae>
2024-02-22 22:51:55 +0400

    Db Changes

--- db.js
+++ db.js
@@ -1,21 +1,13 @@
 const mongoose = require("mongoose");
-// const db = mongoose.connection;
-
-// function connect(MONGO_URL, mongodbOptions = {}) {
-//   console.log("mongo connecting...");
-
-//   mongoose
-//     .connect(MONGO_URL, mongodbOptions)
-//     .catch((err) =>
-//       console.error("Mongo error.....", err.toString(), err.code)
-//     );
-// }
-
 const getConnection = () => {
+try{
+  console.log("mongo connecting...");
   mongoose
-    .connect("mongodb://localhost:27017/imgUpload")
-    .catch((err) =>
-      console.error("Mongo error.....", err.toString(), err.code)
-    );
+    .connect("mongodb://0.0.0.0:27017/imgUpload");
+  }
+    catch(err)  {
+      console.error("Mongo error.....", err.toString(), err.code);
+      
+    }    
 };
 module.exports = { getConnection };
